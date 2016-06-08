64368783e0f8c49458037edcaaece989ba4b0c20
ogom <ogom@outlook.com>
2016-06-09 00:43:27 +0900

    Add config to webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './index',
+  output: {
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'react-hot',
+          'babel'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    port: 4000,
+    inline: true,
+    historyApiFallback: true
+  }
+}
