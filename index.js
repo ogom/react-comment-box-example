5bb4214ef655c9fe37e81753385910807eec0420
ogom <ogom@outlook.com>
2016-06-09 00:48:03 +0900

    Add react tutorial 8

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var CommentBox = require('./app');
 
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
