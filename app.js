eedbf5007f75600b4e95f9103e59892c18e485e2
ogom <ogom@outlook.com>
2016-06-09 00:46:39 +0900

    Add react tutorial 3

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
