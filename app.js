a42b0613c65aee30a53167c43ff505c092bd3c11
ogom <ogom@outlook.com>
2016-06-09 00:49:02 +0900

    Add react tutorial 10

--- app.js
+++ app.js
@@ -23,10 +23,16 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
