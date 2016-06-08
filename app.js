ae775be9ed575015ea1c9fa7c7cd7792f8a1d2fb
ogom <ogom@outlook.com>
2016-06-09 00:46:18 +0900

    Add react tutorial 2

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ var React = require('react');
 var Remarkable = require('remarkable');
 var $ = require('jquery');
 
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: function() {
     return (
