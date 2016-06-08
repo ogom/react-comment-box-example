d618f1e0c0b5a974332689726b3817486b097920
ogom <ogom@outlook.com>
2016-06-09 00:46:55 +0900

    Add react tutorial 4

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ var React = require('react');
 var Remarkable = require('remarkable');
 var $ = require('jquery');
 
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
 var CommentList = React.createClass({
   render: function() {
     return (
