b2a4e8d213a246d8f905d9c7b69db3dc640bec92
ogom <ogom@outlook.com>
2016-06-13 23:48:43 +0900

    Add Provider to ReactRedux

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var CommentBox = require('./app');
 
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var Redux = require('redux');
 var createStore = Redux.createStore;
 var store = createStore(function(state, action) {
@@ -23,13 +18,13 @@ var store = createStore(function(state, action) {
     return state;
   }
 });
-store.subscribe(function () {
-  return console.log(store.getState());
-});
-store.dispatch({type: 'show_comments', comments: data});
-store.dispatch({type: 'add_comment', comment: {author: "foo", text: "*bar*"}});
+
+var ReactRedux = require('react-redux');
+var Provider = ReactRedux.Provider;
 
 ReactDOM.render(
-  <CommentBox url="http://localhost:3000/api/comments" pollInterval={2000} />,
+  <Provider store={store}>
+    <CommentBox url="http://localhost:3000/api/comments" pollInterval={2000} />
+  </Provider>,
   document.getElementById('content')
 );
