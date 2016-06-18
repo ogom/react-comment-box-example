d3adad2e09637c333ca9068581381440cfb91457
ogom <ogom@outlook.com>
2016-06-18 14:08:41 +0900

    Remove semicolons

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var CommentBox = require('./app');
+var React = require('react')
+var ReactDOM = require('react-dom')
+var CommentBox = require('./app')
 
-var Redux = require('redux');
-var createStore = Redux.createStore;
+var Redux = require('redux')
+var createStore = Redux.createStore
 var store = createStore(function(state, action) {
   if (state === undefined) {
-    return [];
+    return []
   }
 
   switch (action.type) {
   case 'show_comments':
-    return action.comments;
+    return action.comments
   case 'add_comment':
-    return state.concat([action.comment]);
+    return state.concat([action.comment])
   default:
-    return state;
+    return state
   }
-});
+})
 
-var ReactRedux = require('react-redux');
-var Provider = ReactRedux.Provider;
+var ReactRedux = require('react-redux')
+var Provider = ReactRedux.Provider
 
 ReactDOM.render(
   <Provider store={store}>
     <CommentBox url="http://localhost:3000/api/comments" pollInterval={2000} />
   </Provider>,
   document.getElementById('content')
-);
+)
