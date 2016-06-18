0f360d8b26ce789c840594f19454f18adf89b7c2
ogom <ogom@outlook.com>
2016-06-18 22:09:50 +0900

    Chenge CommentForm of createClass to extends Component for es2015

--- app.js
+++ app.js
@@ -39,46 +39,51 @@ class CommentList extends Component {
   }
 }
 
-var CommentForm = React.createClass({
-  getInitialState: function() {
-    return {author: '', text: ''}
-  },
-  handleAuthorChange: function(e) {
+class CommentForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {author: '', text: ''}
+  }
+
+  handleAuthorChange(e) {
     this.setState({author: e.target.value})
-  },
-  handleTextChange: function(e) {
+  }
+
+  handleTextChange(e) {
     this.setState({text: e.target.value})
-  },
-  handleSubmit: function(e) {
+  }
+
+  handleSubmit(e) {
     e.preventDefault()
-    var author = this.state.author.trim()
-    var text = this.state.text.trim()
+    const author = this.state.author.trim()
+    const text = this.state.text.trim()
     if (!text || !author) {
       return
     }
     this.props.onCommentSubmit({author: author, text: text})
     this.setState({author: '', text: ''})
-  },
-  render: function() {
+  }
+
+  render() {
     return (
-      <form className="commentForm" onSubmit={this.handleSubmit}>
+      <form className="commentForm" onSubmit={this.handleSubmit.bind(this)}>
         <input
           type="text"
           placeholder="Your name"
           value={this.state.author}
-          onChange={this.handleAuthorChange}
+          onChange={this.handleAuthorChange.bind(this)}
         />
         <input
           type="text"
           placeholder="Say something..."
           value={this.state.text}
-          onChange={this.handleTextChange}
+          onChange={this.handleTextChange.bind(this)}
         />
         <input type="submit" value="Post" />
       </form>
     )
   }
-})
+}
 
 var CommentBox = React.createClass({
   loadCommentsFromServer: function() {
