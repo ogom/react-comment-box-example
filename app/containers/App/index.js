4ac493e3e3f9c1a86f3d5cef8c156d57ad542d56
ogom <ogom@outlook.com>
2016-06-21 22:26:44 +0900

    Move Comment to components

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,26 +1,7 @@
 import React, { Component } from 'react'
-import Remarkable from 'remarkable'
+import Comment from '../../components/Comment'
 import $ from 'jquery'
 import { connect } from 'react-redux'
-
-class Comment extends Component {
-  rawMarkup() {
-    const md = new Remarkable()
-    const rawMarkup = md.render(this.props.children.toString())
-    return { __html: rawMarkup }
-  }
-
-  render() {
-    return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={this.rawMarkup()} />
-      </div>
-    )
-  }
-}
 
 class CommentList extends Component {
   render() {
