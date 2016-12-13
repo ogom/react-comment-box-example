4b2e50621471a6513cb8d865dc498ea249e97e52
ogom <ogom@outlook.com>
2016-12-14 00:26:12 +0900

    Add main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from 'electron'
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
